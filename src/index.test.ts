9a4641aec15504202778c9b7ce885ee58d1ed471
Joe Green <joseph.green@asda.co.uk>
2021-01-06 11:21:25 +0000

    typescript boilerplate

---
+++ src/index.test.ts
@@ -0,0 +1,5 @@
+import { add } from './index';
+
+it('adds two numbers together', () => {
+  expect(add(2, 2)).toEqual(4);
+});
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export const add = (x: number, y: number): number => x + y;
+
+console.log(add(2, 2));
