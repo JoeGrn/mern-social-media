e9ee0937ca243b1586f3eab4f267041f66aa15a4
Joe Green <joseph.green@asda.co.uk>
2021-01-10 14:43:54 +0000

    deletePosts endpoint

--- src/gql/resolvers/posts.ts
+++ src/gql/resolvers/posts.ts
@@ -1,3 +1,5 @@
+import { AuthenticationError } from 'apollo-server';
+
 import Post from '../../models/Post';
 import checkAuth from '../../util/checkAuth';
 
@@ -5,10 +7,10 @@ export default {
   Query: {
     async getPosts() {
       try {
-        const posts = await Post.find();
+        const posts = await Post.find().sort({ createdAt: -1 });
         return posts;
       } catch (e) {
-        console.log(e.message);
+        throw new Error(e.message);
       }
     },
     async getPost(parent: any, args: any) {
@@ -28,7 +30,7 @@ export default {
     async createPost(parent: any, args: any, context: any) {
       const user: any = checkAuth(context);
       const body: any = args.body;
- 
+
       const newPost = new Post({
         body,
         user: user.id,
@@ -39,6 +41,22 @@ export default {
       const post = await newPost.save();
 
       return post;
+    },
+    async deletePost(parent: any, args: any, context: any) {
+      const user: any = checkAuth(context);
+      const postId: any = args.postId;
+
+      try {
+        const post: any = await Post.findById(postId);
+        if (user.username === post.username) {
+          await post.delete();
+          return 'Post Deleted';
+        } else {
+          throw new AuthenticationError('Action not allowed');
+        }
+      } catch (e) {
+        throw new Error(e.message);
+      }
     },
   },
 };
