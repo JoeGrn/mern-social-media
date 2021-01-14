b7309f6a9cab4b4d5e36d6f505aa405e9d7a2fa3
Joe Green <joegreen@Joes-MacBook-Pro.local>
2021-01-14 09:19:54 +0000

    deleteComment

--- src/gql/resolvers/comments.ts
+++ src/gql/resolvers/comments.ts
@@ -1,4 +1,4 @@
-import { UserInputError } from 'apollo-server';
+import { AuthenticationError, UserInputError } from 'apollo-server';
 
 import Post from '../../models/Post';
 import checkAuth from '../../util/checkAuth';
@@ -6,7 +6,7 @@ import checkAuth from '../../util/checkAuth';
 export default {
   Mutation: {
     async createComment(parent: any, args: any, context: any) {
-      const { postId, body } = args.commentInput
+      const { postId, body } = args.commentInput;
       const user: any = checkAuth(context);
 
       if (body.trim() === '') {
@@ -27,6 +27,29 @@ export default {
         await post.save();
 
         return post;
+      } else {
+        throw new UserInputError('Post not found');
+      }
+    },
+    async deleteComment(parent: any, args: any, context: any) {
+      const { postId, commentId } = args;
+      const user: any = checkAuth(context);
+
+      const post: any = await Post.findById(postId);
+
+      if (post) {
+        const commentIndex: any = post.comments.findIndex(
+          (c: any) => c.id === commentId,
+        );
+
+        if (post.comments[commentIndex].username === user.username) {
+          post.comments.splice(commentIndex, 1);
+          await post.save();
+
+          return post;
+        } else {
+          throw new AuthenticationError('Action not allowed');
+        }
       } else {
         throw new UserInputError('Post not found');
       }
