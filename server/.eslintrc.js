8681afd4c45867a3fba160407ce9dd91af0dac09
Joseph Green <joseph.peter.green@gmail.com>
2021-01-15 20:21:10 +0000

    changed eslint.json to .js to fix bug where eslint cannot parse tsconfig in same dir

---
+++ server/.eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: false,
+    es6: true,
+    node: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: tsconfig.json,
+    sourceType: module,
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ['@typescript-eslint', jest],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:jest/recommended',
+    'prettier',
+    'prettier/@typescript-eslint',
+  ],
+  rules: {},
+};
